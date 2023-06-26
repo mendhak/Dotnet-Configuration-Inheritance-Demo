0a356ba0db36c53c2170e34af4cd1ce9fae4e855
mendhak <mendhak@users.noreply.github.com>
2023-06-26 21:32:44 +0100

    Hello world with configuration value and environment setting files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+var currentEnvironment = Environment.GetEnvironmentVariable("ENVIRONMENT_NAME") ?? "local";
+
+
+var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{currentEnvironment}.json", optional: true)
+            .AddEnvironmentVariables();
+var config = builder.Build();
+
+var subject = config["Subject:Name"];
+
+Console.WriteLine($"Hello, {subject}");
